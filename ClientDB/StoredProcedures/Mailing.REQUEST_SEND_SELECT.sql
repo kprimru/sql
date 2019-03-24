5e5154ccb8d32fbc0fd2cf45f7df39dab5906d13
Denisov <hostsql@kprim.ru>
2019-03-25 03:52:10 +1000

    ClientDB подписка на рассылку

---
+++ ClientDB/StoredProcedures/Mailing.REQUEST_SEND_SELECT.sql
@@ -0,0 +1,25 @@
+USE [ClientDB]
+	GO
+	SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [Mailing].[REQUEST_SEND_SELECT]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		[ID]			= R.[ID],
+		[FromAddress]	= 'no-reply@kprim.ru', 
+		[FromName]		= 'ООО Базис', 
+		[Psedo]			= SH_NAME,
+		[Email]			= SH_EMAIL,
+		[Subj]			= 'Клиент подписался на рассылку',
+		[Body]			= 'Клиент "' + RN.Comment + '" (' + RN.DistrStr + ') подал заявку на подписку на рассылку' 
+	FROM Mailing.Requests R
+	INNER JOIN Reg.RegNodeSearchView RN WITH(NOEXPAND) ON RN.HostId = R.HostID AND RN.DistrNumber = R.Distr AND RN.CompNumber = R.Comp
+	INNER JOIN dbo.Subhost S ON RN.SubhostName = S.SH_REG
+	WHERE R.SendDate IS NULL
+		AND SH_EMAIL != ''
+END
