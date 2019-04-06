8e26867ecce43a74464ae4ed27ba625c8aea1e69
Denisov <hostsql@kprim.ru>
2019-04-06 17:18:06 +1000

    IPLogs. Табуляция

--- IPLogs/StoredProcedures/dbo.SEND_ADM_PK_KRAY_CITY_REPORT.sql
+++ IPLogs/StoredProcedures/dbo.SEND_ADM_PK_KRAY_CITY_REPORT.sql
@@ -1,10 +1,10 @@
 USE [IPLogs]
-	GO
-	SET ANSI_NULLS ON
-	GO
-	SET QUOTED_IDENTIFIER ON
-	GO
-	-- =============================================
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
