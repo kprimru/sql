09d0d638ff791d2a47ff30dba85a2d25b70bb9d5
Evgeny <sakhjen@gmail.com>
2019-05-24 13:49:59 +1000

    Фикс для отображения списка сущностей, содержащих запятые и дробные числа в ИМ

--- ClientDB/StoredProcedures/dbo.NAMED_SET_SELECT.sql
+++ ClientDB/StoredProcedures/dbo.NAMED_SET_SELECT.sql
@@ -10,25 +10,47 @@ WITH EXECUTE AS OWNER
 AS
 BEGIN
 	SET NOCOUNT ON
-	DECLARE @SQL	NVARCHAR(MAX)
+	DECLARE @SQL		NVARCHAR(MAX)
+	DECLARE @VALUE_TYPE	NVARCHAR(20)
+
+	SELECT @VALUE_TYPE=DATA_TYPE
+	FROM information_schema.COLUMNS
+	WHERE TABLE_SCHEMA=LEFT(@REFNAME, CHARINDEX('.', @REFNAME)-1)
+		AND TABLE_NAME=RIGHT(@REFNAME, (LEN(@REFNAME)-CHARINDEX('.', @REFNAME)))
+		AND COLUMN_NAME=(	SELECT ValueColumn
+							FROM dbo.RefColumnMeta
+							WHERE RefName=@REFNAME)
+
 	SELECT @SQL='SELECT
 					SetName,
-					(SELECT STUFF(CAST((
-							SELECT '', ''+CAST(('+ ValueColumn
-										+') AS VARCHAR)
-							FROM '+@REFNAME+'
-							WHERE '+IDColumn+' IN (SELECT CAST(NSI.SetItem AS NVARCHAR)
-													FROM dbo.NamedSetsItems NSI
-													WHERE NSI.SetId=NS.SetId)
-					FOR XML PATH(''''), TYPE) AS VARCHAR(MAX)), 1, 2, '''')
-					) AS ''ItemList''
-				FROM dbo.NamedSets NS
-				WHERE RefName='''+@REFNAME+''''
+					(SELECT STUFF(CAST(('
+					+
+					CASE																--для того чтобы дробные числа отображались с запятой, а не с точкой
+					WHEN @VALUE_TYPE='float' OR @VALUE_TYPE='real'
+					THEN		
+						'SELECT '', ''+REPLACE(CAST(('+ ValueColumn +') AS VARCHAR(MAX)),''.'', '','')'
+					WHEN @REFNAME='Training.TrainingSubject'
+					THEN		
+						'SELECT '', ''+REPLACE(CAST(('+ ValueColumn +') AS VARCHAR(MAX)),'','', '',.'')'			
+					ELSE
+						'SELECT '', ''+CAST(('+ ValueColumn +') AS VARCHAR(MAX))'
+					END
+					+
+					'FROM '+@REFNAME+'
+								WHERE '+IDColumn+' IN (SELECT CAST(NSI.SetItem AS NVARCHAR(256))
+														FROM dbo.NamedSetsItems NSI
+														WHERE NSI.SetId=NS.SetId)
+								FOR XML PATH(''''), TYPE) AS VARCHAR(MAX)), 1, 2, '''')
+						) AS ''ItemList''
+						FROM dbo.NamedSets NS
+						WHERE RefName='''+@REFNAME+''''	
 	FROM 
 		dbo.NamedSets NS
 		INNER JOIN dbo.RefColumnMeta RCM ON RCM.RefName=NS.RefName
 	WHERE
 		NS.RefName=@REFNAME
+
+	--PRINT(@VALUE_TYPE)
 	--PRINT @SQL
 	EXEC (@SQL)
 END
