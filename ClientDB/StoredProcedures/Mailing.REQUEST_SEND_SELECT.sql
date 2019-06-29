3f7fed879249bd16ff6377c840e007e8fa8a917b
Denisov <hostsql@kprim.ru>
2019-06-29 14:38:18 +1000

    ClientDB. При отправке письма о рассылке подхостам в текст включен E-Mail

--- ClientDB/StoredProcedures/Mailing.REQUEST_SEND_SELECT.sql
+++ ClientDB/StoredProcedures/Mailing.REQUEST_SEND_SELECT.sql
@@ -16,7 +16,7 @@ BEGIN
 		[Psedo]			= SH_NAME,
 		[Email]			= SH_EMAIL,
 		[Subj]			= 'Клиент подписался на рассылку',
-		[Body]			= 'Клиент "' + RN.Comment + '" (' + RN.DistrStr + ') подал заявку на подписку на рассылку' 
+		[Body]			= 'Клиент "' + RN.Comment + '" (' + RN.DistrStr + ') подал заявку на подписку на рассылку на адрес "' + IsNull(R.[OriginalEMail], '') + '"'
 	FROM Mailing.Requests R
 	INNER JOIN Reg.RegNodeSearchView RN WITH(NOEXPAND) ON RN.HostId = R.HostID AND RN.DistrNumber = R.Distr AND RN.CompNumber = R.Comp
 	INNER JOIN dbo.Subhost S ON RN.SubhostName = S.SH_REG
