81217e4d4b07b87d0679ac7e90b29e830b4ed727
Denisov <hostsql@kprim.ru>
2019-08-11 16:15:34 +1000

    ClientDB. Легкий рефакторинг

--- ClientDB/StoredProcedures/dbo.BL_GET_CLIENT_SYSTEMS_ALL.sql
+++ ClientDB/StoredProcedures/dbo.BL_GET_CLIENT_SYSTEMS_ALL.sql
@@ -14,22 +14,17 @@ BEGIN
 
 	SELECT 
 		f.ID, 
-		SystemShortName, a.ID_SYSTEM AS SystemID, b.SystemBaseName,
+		SystemShortName, a.SystemID, a.SystemBaseName,
         DISTR AS SystemDistrNumber, COMP AS CompNumber,
 		SystemTypeName,	
         DistrTypeName, 
 		DS_NAME AS ServiceStatusName,
         f.Complect
 	FROM
-		dbo.ClientDistr a INNER JOIN	
-		dbo.SystemTable b ON a.ID_SYSTEM = b.SystemID INNER JOIN
-		dbo.SystemTypeTable c ON c.SystemTypeID = a.ID_TYPE INNER JOIN
-		dbo.DistrTypeTable d ON d.DistrTypeID = a.ID_NET INNER JOIN
-		--ServiceStatusTable e ON e.ServiceStatusID = a.SystemStatusID LEFT OUTER JOIN
-		dbo.DistrStatus ON DS_ID = ID_STATUS LEFT OUTER JOIN
-		dbo.RegNodeTable f ON f.SystemName = b.SystemBaseName 
-						AND f.DistrNumber = a.DISTR
-						AND f.CompNumber = a.COMP
-	WHERE  ID_CLIENT = @clientid AND a.STATUS = 1
+		dbo.ClientDistrView a WITH(NOEXPAND)
+		INNER JOIN dbo.RegNodeTable f ON	f.SystemName = a.SystemBaseName 
+										AND f.DistrNumber = a.DISTR
+										AND f.CompNumber = a.COMP
+	WHERE  ID_CLIENT = @clientid
 	ORDER BY DS_INDEX, SystemOrder
 END
