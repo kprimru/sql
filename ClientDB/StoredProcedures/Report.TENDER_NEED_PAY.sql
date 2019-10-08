d30371c6784c6aee8400106598f37edf7d23075e
Evgeny <sakhjen@gmail.com>
2019-10-09 09:05:19 +1000

    Неф. отчеты по тендерам для Литвин

---
+++ ClientDB/StoredProcedures/Report.TENDER_NEED_PAY.sql
@@ -0,0 +1,39 @@
+USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [Report].[TENDER_NEED_PAY]
+	@PARAM	NVARCHAR(MAX) = NULL
+AS
+BEGIN
+	SELECT 
+		t.CLIENT AS [Наименование заказчика],
+		SHORT AS [Базис/К-Прим],
+		p.CLAIM_PRIVISION AS [Сумма обеспечения заявки],
+		CASE DATEPART(dw, p.DATE)
+			WHEN 1 THEN p.DATE+2
+			WHEN 2 THEN p.DATE+1
+			WHEN 3 THEN p.DATE+2
+			WHEN 4 THEN p.DATE+1
+			WHEN 5 THEN p.DATE+5
+			WHEN 6 THEN p.DATE+4
+			WHEN 7 THEN p.DATE+3
+		END AS [Срок оплаты],
+		TS_NAME AS [Эл. пл.],
+		GK_SUM AS [НМЦК],
+		NOTICE_NUM AS [Номер извещения],
+		DATE AS [Дата извещения]
+	FROM 
+		Tender.Tender t
+		INNER JOIN Tender.Placement p ON t.ID = p.ID_TENDER
+		INNER JOIN dbo.Vendor v ON p.ID_VENDOR = v.ID
+		INNER JOIN Purchase.TradeSite TS on p.ID_TRADESITE = ts.TS_ID
+	WHERE ID_STATUS = (
+					SELECT ID
+					FROM Tender.Status
+					WHERE PSEDO = 'TENDER'
+						)
+	ORDER BY DATE DESC
+END
