9f3260440ca7974121a9da876c7ffa8e802717fd
Evgeny <sakhjen@gmail.com>
2019-11-07 11:58:59 +1000

    ClientDB. Доработаны процедуры сохранения с учетом новых полей для типов договоров

--- ClientDB/StoredProcedures/Contract.CLIENT_CONTRACT_SELECT.sql
+++ ClientDB/StoredProcedures/Contract.CLIENT_CONTRACT_SELECT.sql
@@ -5,31 +5,21 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE PROCEDURE [Contract].[CLIENT_CONTRACT_SELECT]
-	@CLIENT	INT,
-	@ADD	BIT,
-	@SPEC	BIT
+	@ClientId	Int
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	IF OBJECT_ID('tempdb..#contract') IS NOT NULL
-		DROP TABLE #contract
-	
-	CREATE TABLE #contract(ID UNIQUEIDENTIFIER PRIMARY KEY)
-	
-	INSERT INTO #contract(ID)
-		SELECT DISTINCT ID_CONTRACT
-		FROM 
-			Contract.ClientContract a
-		WHERE a.ID_CLIENT = @CLIENT
-		
-	SELECT b.ID, c.NAME, b.NUM_S, b.DATE, b.NOTE
-	FROM 
-		#contract a
-		INNER JOIN Contract.Contract b ON a.ID = b.ID
-		INNER JOIN dbo.Vendor c ON c.ID = b.ID_VENDOR
-		
-	
-	IF OBJECT_ID('tempdb..#contract') IS NOT NULL
-		DROP TABLE #contract
+	SELECT C.ID, C.NUM_S, DateFrom, DateTo, ExpireDate, Type_Id, PayType_Id, Discount_Id, ContractPrice, Comments
+	FROM Contract.ClientContracts	CC
+	INNER JOIN Contract.Contract	C	ON C.ID = CC.Contract_Id
+	CROSS APPLY
+	(
+		SELECT TOP (1) ExpireDate, Type_Id, PayType_Id, Discount_Id, ContractPrice, Comments
+		FROM Contract.ClientContractsDetails CCD
+		WHERE CCD.Contract_Id = CC.Contract_Id
+		ORDER BY CCD.DATE DESC
+	) D
+	WHERE CC.Client_Id = @ClientId
+	ORDER BY DateFrom DESC
 END
