40b0e51f43494a0902c6216355563010c7bddd32
Denisov <hostsql@kprim.ru>
2019-11-10 02:00:29 +1000

    ClientDB. При любом изменении в дистрибутивах - пересчитывать тип клиента

---
+++ ClientDB/Triggers/dbo.ClientDistr Insert_Update_Delete.sql
@@ -0,0 +1,32 @@
+USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [dbo].[ClientDistr Insert/Update/Delete] ON  dbo.ClientDistr
+   AFTER INSERT,DELETE,UPDATE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+
+    DECLARE @IDs VarChar(Max);
+    
+    SET @IDs = REVERSE(STUFF(REVERSE(
+		(
+			SELECT Cast(ID_CLIENT AS VarChar(20)) + ','
+			FROM
+			(
+				SELECT ID_CLIENT
+				FROM inserted
+			    
+				UNION
+			    
+				SELECT ID_CLIENT
+				FROM deleted
+			) I
+			FOR XML PATH('')
+		)), 1, 1, ''));
+		
+	EXEC dbo.CLIENT_TYPE_RECALCULATE @IDs;
+END
