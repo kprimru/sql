09ad190d19b7012c94f351d0961b758ddc639274
Denisov <hostsql@kprim.ru>
2020-02-15 17:38:01 +1000

    ClientDB. Отправка ЗВЭ подхостам. Сразу все подхосты в одной процедуре

--- ClientDB/StoredProcedures/dbo.EXPERT_QUESTION_SUBHOST.sql
+++ ClientDB/StoredProcedures/dbo.EXPERT_QUESTION_SUBHOST.sql
@@ -5,24 +5,13 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE PROCEDURE [dbo].[EXPERT_QUESTION_SUBHOST]
-	@SUBHOST	UNIQUEIDENTIFIER
+	@SUBHOST	UNIQUEIDENTIFIER = NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @SH_REG NVARCHAR(16)
-	DECLARE @SH_REG_ADD NVARCHAR(16)
-	DECLARE @SH_EMAIL NVARCHAR(128)
-
-	SELECT @SH_REG = SH_REG, @SH_REG_ADD = SH_REG_ADD, @SH_EMAIL = SH_EMAIL
-	FROM dbo.Subhost
-	WHERE SH_ID = @SUBHOST
-
-	SET @SH_REG = '(' + @SH_REG + ')%'
-	SET @SH_REG_ADD = '(' + @SH_REG_ADD + ')%'
-
 	SELECT 
-		ID, SYS, DISTR, COMP, DATE, FIO, EMAIL, PHONE, QUEST, @SH_EMAIL AS SH_EMAIL,
+		ID, SYS, DISTR, COMP, DATE, FIO, EMAIL, PHONE, QUEST, SH_EMAIL,
 		CONVERT(NVARCHAR(16), SYS) + '_' + CONVERT(NVARCHAR(16), DISTR) + 
 			CASE COMP 
 				WHEN 1 THEN '' 
@@ -37,44 +26,53 @@ BEGIN
 			FOR XML PATH('quest'), ROOT('root')
 		) AS QUEST_XML
 	FROM
-		(
-			SELECT SystemName, DistrNumber, CompNumber
-			FROM
-				(
-					SELECT a.SystemName, DistrNumber, CompNumber
-					FROM dbo.RegNodeTable a
-					WHERE Comment LIKE @SH_REG OR Comment LIKE @SH_REG_ADD
+	(
+		SELECT
+			SH_REG		= '(' + SH_REG + ')%',
+			SH_REG_ADD	= '(' + SH_REG_ADD + ')%',
+			SH_EMAIL	= SH_EMAIL
+		FROM dbo.Subhost
+		WHERE SH_REG IN ('Ì', 'Ó1', 'Í1')
+	) SH
+	CROSS APPLY
+	(
+		SELECT SystemName, DistrNumber, CompNumber
+		FROM
+			(
+				SELECT a.SystemName, DistrNumber, CompNumber
+				FROM dbo.RegNodeTable a
+				WHERE Comment LIKE SH_REG OR Comment LIKE SH_REG_ADD
 
-					UNION
+				UNION
 
-					SELECT a.SystemName, DistrNumber, CompNumber
-					FROM 
-						dbo.RegNodeTable a
-						INNER JOIN dbo.SystemTable b ON a.SystemName = b.SystemBaseName
-						INNER JOIN dbo.SubhostComplect c ON SC_DISTR = DistrNumber AND SC_COMP = CompNumber AND c.SC_ID_HOST = b.HostID
-					WHERE SystemReg = 1 AND SC_REG = 1 AND SC_ID_SUBHOST = @SUBHOST
+				SELECT a.SystemName, DistrNumber, CompNumber
+				FROM 
+					dbo.RegNodeTable a
+					INNER JOIN dbo.SystemTable b ON a.SystemName = b.SystemBaseName
+					INNER JOIN dbo.SubhostComplect c ON SC_DISTR = DistrNumber AND SC_COMP = CompNumber AND c.SC_ID_HOST = b.HostID
+				WHERE SystemReg = 1 AND SC_REG = 1 AND SC_ID_SUBHOST = @SUBHOST
 
-					UNION 
+				UNION 
 
-					SELECT SystemName, DistrNumber, CompNumber
-					FROM dbo.RegNodeTable
-					WHERE Complect IN
-						(
-							SELECT Complect
-							FROM 
-								dbo.RegNodeTable a
-								INNER JOIN dbo.SystemTable b ON a.SystemName = b.SystemBaseName
-								INNER JOIN dbo.SubhostComplect c ON SC_DISTR = DistrNumber AND SC_COMP = CompNumber AND c.SC_ID_HOST = b.HostID
-							WHERE SystemReg = 1 AND SC_REG = 1 AND SC_ID_SUBHOST = @SUBHOST
-						)
+				SELECT SystemName, DistrNumber, CompNumber
+				FROM dbo.RegNodeTable
+				WHERE Complect IN
+					(
+						SELECT Complect
+						FROM 
+							dbo.RegNodeTable a
+							INNER JOIN dbo.SystemTable b ON a.SystemName = b.SystemBaseName
+							INNER JOIN dbo.SubhostComplect c ON SC_DISTR = DistrNumber AND SC_COMP = CompNumber AND c.SC_ID_HOST = b.HostID
+						WHERE SystemReg = 1 AND SC_REG = 1 AND SC_ID_SUBHOST = @SUBHOST
+					)
+				
+				UNION
 					
-					UNION
-						
-					SELECT a.SystemName, DistrNumber, CompNumber
-					FROM dbo.RegNodeTable a
-					WHERE Comment LIKE '%' + @SH_REG
-				) AS o_O
-			) AS a
+				SELECT a.SystemName, DistrNumber, CompNumber
+				FROM dbo.RegNodeTable a
+				WHERE Comment LIKE '%' + SH_REG
+			) AS o_O
+		) AS a
 			INNER JOIN dbo.SystemTable b ON b.SystemBaseName = a.SystemName
 			INNER JOIN dbo.SystemTable c ON b.HostID = c.HostID
 			INNER JOIN dbo.ClientDutyQuestion d ON d.SYS = c.SystemNumber AND d.DISTR = a.DistrNumber AND d.COMP = a.CompNumber
