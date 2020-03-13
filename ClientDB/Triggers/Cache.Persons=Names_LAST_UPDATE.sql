e97ec1acc448fef0ac755ae7fb95c53cb5bf3fdf
Denisov <hostsql@kprim.ru>
2020-03-13 15:34:47 +1000

    ClientDB. Кэш ФИО как справочники (с LAST_UDPATE)

---
+++ ClientDB/Triggers/Cache.Persons=Names_LAST_UPDATE.sql
@@ -0,0 +1,21 @@
+USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [Cache].[Persons=Names_LAST_UPDATE] ON [Cache].[Persons=Names]
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	UPDATE Common.Reference
+	SET ReferenceLast = GETDATE()
+	WHERE ReferenceName = 'Persons=Names'
+		AND ReferenceSchema = 'Cache';
+	
+	IF @@RowCount = 0
+		INSERT INTO Common.Reference(ReferenceSchema, ReferenceName, ReferenceLast)
+		SELECT 'Cache', 'Persons=Names', GetDate();
+END
---
+++ ClientDB/Triggers/Cache.Persons=Positions_LAST_UPDATE.sql
@@ -0,0 +1,21 @@
+USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [Cache].[Persons=Positions_LAST_UPDATE] ON [Cache].[Persons=Positions]
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	UPDATE Common.Reference
+	SET ReferenceLast = GETDATE()
+	WHERE ReferenceName = 'Persons=Positions'
+		AND ReferenceSchema = 'Cache';
+	
+	IF @@RowCount = 0
+		INSERT INTO Common.Reference(ReferenceSchema, ReferenceName, ReferenceLast)
+		SELECT 'Cache', 'Persons=Positions', GetDate();
+END
