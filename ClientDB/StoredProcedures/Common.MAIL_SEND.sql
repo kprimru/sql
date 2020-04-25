5885e431673899a27af5da6b22e5498bfd9f653e
Denisov <hostsql@kprim.ru>
2020-04-25 16:22:36 +1000

    ClientDB. Возможность записи вопросов на семинары

---
+++ ClientDB/StoredProcedures/Common.MAIL_SEND.sql
@@ -0,0 +1,44 @@
+USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [Common].[MAIL_SEND]
+	@Recipients	NVarChar(Max),
+	@Subject	NVarChar(Max),
+	@Body		NVarChar(Max)
+WITH EXECUTE AS OWNER
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE
+		@DebugError		VarChar(512),
+		@DebugContext	Xml,
+		@Params			Xml;
+
+	EXEC [Debug].[Execution@Start]
+		@Proc_Id		= @@ProcId,
+		@Params			= @Params,
+		@DebugContext	= @DebugContext OUT
+
+	BEGIN TRY
+
+		EXEC msdb.dbo.sp_send_dbmail 
+					@profile_name	=	'SQLMail',
+					@recipients		=	@Recipients,
+					@body			=	@Body,
+					@subject		=	@Subject,
+					@query_result_header	=	0				
+					
+		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = NULL;
+	END TRY
+	BEGIN CATCH
+		SET @DebugError = Error_Message();
+		
+		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = @DebugError;
+		
+		EXEC [Maintenance].[ReRaise Error];
+	END CATCH
+END
