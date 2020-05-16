f7d3aabbf03b05ff252a21b17378aea25c996343
Denisov <hostsql@kprim.ru>
2020-05-16 15:12:08 +1000

    SaleDB. Этап -> Депо

--- SaleDB/StoredProcedures/Client.CompanyDepo@Set Number.sql
+++ SaleDB/StoredProcedures/Client.CompanyDepo@Set Number.sql
@@ -5,20 +5,23 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER PROCEDURE [Client].[CompanyDepo@Set Number]
-	@Id		UniqueIdentifier,
-	@Number	Int
+    @Id     UniqueIdentifier,
+    @Number Int
 AS
 BEGIN
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-	BEGIN TRY
-		UPDATE Client.CompanyDepo
-		SET [Number] = @Number
-		WHERE [Id] = @Id
-	END TRY
-	BEGIN CATCH
-		EXEC [Maintenance].[ReRaise Error];
-	END CATCH
+    BEGIN TRY
+        IF EXISTS(SELECT * FROM Client.CompanyDepo WHERE [Number] = @Number AND [Status] = 1 AND [Id] != @Id)
+            RaisError('Ошибка! Данный номер уже используется!', 16, 1);
+
+        UPDATE Client.CompanyDepo
+        SET [Number] = @Number
+        WHERE [Id] = @Id
+    END TRY
+    BEGIN CATCH
+        EXEC [Maintenance].[ReRaise Error];
+    END CATCH
 END
 GO
 GRANT EXECUTE ON [Client].[CompanyDepo@Set Number] TO rl_depo_number;
