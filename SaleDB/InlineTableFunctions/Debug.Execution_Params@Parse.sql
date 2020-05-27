be335e9310daf7d0045e3c34ec35095b80b55dba
Denisov <hostsql@kprim.ru>
2020-05-27 15:12:16 +1000

    SaleDB. Debug@Poing и Debug@Read

---
+++ SaleDB/InlineTableFunctions/Debug.Execution_Params@Parse.sql
@@ -0,0 +1,20 @@
+USE [SaleDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER FUNCTION [Debug].[Execution:Params@Parse]
+(
+	@Params Xml
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT
+		[Row:Index] = 1,
+		[Name]		= 'Name',
+		[Value]		= 'Value'
+)
+GO
