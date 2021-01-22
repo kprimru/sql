c33bca46bcfd225b31858c59de2baa27d8a6ec5f
Denisov <hostsql@kprim.ru>
2021-01-23 03:01:39 +1000

    ClientDB. Debug Params

--- ClientDB/InlineTableFunctions/Debug.Execution_Params@Parse.sql
+++ ClientDB/InlineTableFunctions/Debug.Execution_Params@Parse.sql
@@ -16,5 +16,14 @@ RETURN
 		[Row:Index] = 1,
 		[Name]		= 'Name',
 		[Value]		= 'Value'
+	WHERE @Params IS NULL
+
+	UNION ALL
+
+	SELECT
+        Row_Number() OVER(ORDER BY (SELECT 1)),
+        v.value('@Name[1]', 'VarChar(256)'),
+        v.value('@Value[1]', 'VarChar(Max)')
+    FROM @Params.nodes('/PARAMS/PARAM') AS n(v)
 )
 GO
