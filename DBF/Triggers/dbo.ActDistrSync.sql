382366a1d8a419401bcd2c12d74e30ac41447d58
Denisov <hostsql@kprim.ru>
2021-02-13 16:02:47 +1000

    DBF. Синхронизация фин.данных без периода

--- DBF/Triggers/dbo.ActDistrSync.sql
+++ DBF/Triggers/dbo.ActDistrSync.sql
@@ -9,54 +9,48 @@ ALTER TRIGGER [dbo].[ActDistrSync]
    AFTER INSERT, UPDATE, DELETE
 AS
 BEGIN
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
     DECLARE @Distrs Table
     (
-		SYS_REG_NAME	VarChar(20),
-		DIS_NUM			Int,
-		DIS_COMP_NUM	TinyInt,
-		PR_DATE			SmallDateTime,
-		Primary Key Clustered (DIS_NUM, SYS_REG_NAME, PR_DATE, DIS_COMP_NUM)
+        SYS_REG_NAME    VarChar(20),
+        DIS_NUM         Int,
+        DIS_COMP_NUM    TinyInt
+        Primary Key Clustered (DIS_NUM, SYS_REG_NAME, DIS_COMP_NUM)
     );
 
     INSERT INTO @Distrs
-	SELECT DISTINCT SYS_REG_NAME, DIS_NUM, DIS_COMP_NUM, PR_DATE
-	FROM
-	(
-		SELECT AD_ID_DISTR, AD_ID_PERIOD
-		FROM inserted I
+    SELECT DISTINCT SYS_REG_NAME, DIS_NUM, DIS_COMP_NUM
+    FROM
+    (
+        SELECT AD_ID_DISTR
+        FROM inserted I
 
-		UNION
+        UNION
 
-		SELECT AD_ID_DISTR, AD_ID_PERIOD
-		FROM deleted D
-	) T
-	INNER JOIN dbo.DistrTable D ON D.DIS_ID = T.AD_ID_DISTR
-	INNER JOIN dbo.SystemTable S ON S.SYS_ID = D.DIS_ID_SYSTEM
-	INNER JOIN dbo.PeriodTable P ON P.PR_ID = T.AD_ID_PERIOD;
+        SELECT AD_ID_DISTR
+        FROM deleted D
+    ) T
+    INNER JOIN dbo.DistrTable D ON D.DIS_ID = T.AD_ID_DISTR
+    INNER JOIN dbo.SystemTable S ON S.SYS_ID = D.DIS_ID_SYSTEM;
 
-	UPDATE S
-	SET UPD_DATE = GetDate()
-	FROM Sync.DistrFinancing	S
-	INNER JOIN @Distrs			D ON	D.SYS_REG_NAME	= S.SYS_REG_NAME
-									AND	D.DIS_NUM		= S.DIS_NUM
-									AND D.DIS_COMP_NUM	= S.DIS_COMP_NUM
-									AND D.PR_DATE		= S.PR_DATE;
+    UPDATE S
+    SET UPD_DATE = GetDate()
+    FROM Sync.DistrFinancing    S
+    INNER JOIN @Distrs          D ON    D.SYS_REG_NAME  = S.SYS_REG_NAME
+                                    AND	D.DIS_NUM       = S.DIS_NUM
+                                    AND D.DIS_COMP_NUM  = S.DIS_COMP_NUM;
 
-	INSERT INTO Sync.DistrFinancing(SYS_REG_NAME, DIS_NUM, DIS_COMP_NUM, PR_DATE)
-	SELECT SYS_REG_NAME, DIS_NUM, DIS_COMP_NUM, PR_DATE
-	FROM @Distrs D
-	WHERE NOT EXISTS
-		(
-			SELECT *
-			FROM Sync.DistrFinancing S
-			WHERE	D.SYS_REG_NAME	= S.SYS_REG_NAME
-				AND	D.DIS_NUM		= S.DIS_NUM
-				AND D.DIS_COMP_NUM	= S.DIS_COMP_NUM
-				AND D.PR_DATE		= S.PR_DATE
-		);
+    INSERT INTO Sync.DistrFinancing(SYS_REG_NAME, DIS_NUM, DIS_COMP_NUM)
+    SELECT SYS_REG_NAME, DIS_NUM, DIS_COMP_NUM
+    FROM @Distrs D
+    WHERE NOT EXISTS
+        (
+            SELECT *
+            FROM Sync.DistrFinancing S
+            WHERE	D.SYS_REG_NAME  = S.SYS_REG_NAME
+                AND D.DIS_NUM       = S.DIS_NUM
+                AND D.DIS_COMP_NUM  = S.DIS_COMP_NUM
+        );
 END
-
-
 GO
