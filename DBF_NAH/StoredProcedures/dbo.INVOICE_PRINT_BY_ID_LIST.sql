dbe02cc585fc708993178d7678ffb1a07584ffaa
Denisov <hostsql@kprim.ru>
2021-08-08 14:33:08 +1000

    DBF. Новая форма счет-фактуры

--- DBF_NAH/StoredProcedures/dbo.INVOICE_PRINT_BY_ID_LIST.sql
+++ DBF_NAH/StoredProcedures/dbo.INVOICE_PRINT_BY_ID_LIST.sql
@@ -14,7 +14,9 @@ GO
 ALTER PROCEDURE [dbo].[INVOICE_PRINT_BY_ID_LIST]
 	@invid VARCHAR(MAX),
 	@preview BIT = 1,
-	@group BIT = 0
+	@group BIT = 0,
+	@print_master Bit  = 1,
+	@print_detail Bit = 1
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -37,17 +39,18 @@ BEGIN
 
 		CREATE TABLE #inv
 			(
-				INV_ID INT
+				INV_ID INT PRIMARY KEY CLUSTERED
 			)
 
 		INSERT INTO #inv
-			SELECT * FROM dbo.GET_TABLE_FROM_LIST(@invid, ',')
+		SELECT DISTINCT *
+		FROM dbo.GET_TABLE_FROM_LIST(@invid, ',');
 
 		INSERT INTO dbo.FinancingProtocol(ID_CLIENT, ID_DOCUMENT, TP, OPER, TXT)
-			SELECT INS_ID_CLIENT, INS_ID, 'INVOICE', 'Печать с/ф', '№' + CONVERT(VARCHAR(20), INS_NUM) + '/' + CONVERT(VARCHAR(20), INS_NUM_YEAR)
-			FROM
-				dbo.InvoiceSaleTable
-				INNER JOIN #inv ON INS_ID = INV_ID
+		SELECT INS_ID_CLIENT, INS_ID, 'INVOICE', 'Печать с/ф', '№' + CONVERT(VARCHAR(20), INS_NUM) + '/' + CONVERT(VARCHAR(20), INS_NUM_YEAR)
+		FROM
+			dbo.InvoiceSaleTable
+			INNER JOIN #inv ON INS_ID = INV_ID
 
 		DECLARE @date DATETIME
 		SET @date = GETDATE()
@@ -84,7 +87,8 @@ BEGIN
 				INS_CONSIG_NAME VARCHAR(500),
 				INS_ID_TYPE SMALLINT,
 				COUR_NAME VARCHAR(100),
-				INS_IDENT NVARCHAR(128)
+				INS_IDENT NVARCHAR(128),
+				ACT_DATE    SmallDateTime
 			)
 
 		INSERT INTO #master (
@@ -97,7 +101,7 @@ BEGIN
 						ORG_DIR_SHORT, ORG_BUH_SHORT,
 						CL_ID, CL_PSEDO, CL_FULL_NAME,
 						CL_SHORT_NAME, CL_KPP, CL_INN, INS_CLIENT_ADDR,
-						INS_CONSIG_ADDR, INS_CONSIG_NAME, INS_ID_TYPE, COUR_NAME, INS_IDENT
+						INS_CONSIG_ADDR, INS_CONSIG_NAME, INS_ID_TYPE, COUR_NAME, INS_IDENT, ACT_DATE
 					)
 			SELECT
 					@date,
@@ -118,11 +122,18 @@ BEGIN
 						WHERE TO_ID_CLIENT = INS_ID_CLIENT
 						ORDER BY TO_MAIN DESC
 					),
-					INS_IDENT
+					INS_IDENT, ACT_DATE
 				FROM	dbo.InvoiceSaleTable	A									INNER JOIN
 						#inv				B	ON	A.INS_ID = B.INV_ID			INNER JOIN
 						dbo.OrganizationView	C	ON	A.INS_ID_ORG=C.ORG_ID		INNER JOIN
 						dbo.ClientTable			D	ON	ISNULL(A.INS_ID_PAYER, A.INS_ID_CLIENT)=D.CL_ID
+						OUTER APPLY
+						(
+						    SELECT TOP (1)
+						        ACT_DATE
+						    FROM dbo.ActTable
+						    WHERE ACT_ID_INVOICE = A.INS_ID
+						) AS ACT
 
 		IF OBJECT_ID('tempdb..#detail') IS NOT NULL
 			DROP TABLE #detail
@@ -264,124 +275,132 @@ BEGIN
 
 		IF @GROUP = 1
 		BEGIN
-			SELECT
-				IFM_ID,IFM_DATE,INS_ID,
-				--INS_ID_TYPE,
-				INT_PSEDO,
-				ORG_ID,ORG_PSEDO,ORG_FULL_NAME,ORG_SHORT_NAME,ORG_ADDRESS,ORG_INN,ORG_KPP,
-				INS_DATE,INS_NUM,INS_NUM_YEAR,
-				CL_ID,CL_PSEDO,CL_FULL_NAME,CL_SHORT_NAME,CL_INN,CL_KPP,
-				INS_CLIENT_ADDR,INS_CONSIG_NAME,INS_CONSIG_ADDR,INS_DOC_STRING,
-				ORG_DIR_SHORT,ORG_BUH_SHORT, INS_IDENT
-			FROM
-				#master AS IFM INNER JOIN
-				#inv ON INV_ID = INS_ID
-				LEFT JOIN dbo.InvoiceTypeTable AS ITP ON IFM.INS_ID_TYPE=ITP.INT_ID
-			WHERE IFM_DATE = @date
-			ORDER BY COUR_NAME, CL_PSEDO, CL_ID
+		    IF @Print_master = 1
+			    SELECT
+				    IFM_ID,IFM_DATE,INS_ID,
+				    --INS_ID_TYPE,
+				    INT_PSEDO,
+				    ORG_ID,ORG_PSEDO,ORG_FULL_NAME,ORG_SHORT_NAME,ORG_ADDRESS,ORG_INN,ORG_KPP,
+				    INS_DATE,INS_NUM,INS_NUM_YEAR,
+				    CL_ID,CL_PSEDO,CL_FULL_NAME,CL_SHORT_NAME,CL_INN,CL_KPP,
+				    INS_CLIENT_ADDR,INS_CONSIG_NAME,INS_CONSIG_ADDR,INS_DOC_STRING,
+				    ORG_DIR_SHORT,ORG_BUH_SHORT, INS_IDENT, ACT_DATE, Convert(VarChar(20), ACT_DATE, 104) AS ACT_DATE_S
+			    FROM
+				    #master AS IFM INNER JOIN
+				    #inv ON INV_ID = INS_ID
+				    LEFT JOIN dbo.InvoiceTypeTable AS ITP ON IFM.INS_ID_TYPE=ITP.INT_ID
+			    WHERE IFM_DATE = @date
+			    ORDER BY COUR_NAME, CL_PSEDO, CL_ID
 
-			IF (SELECT TOP 1 INS_ID_TYPE FROM #master) = (SELECT INT_ID FROM dbo.InvoiceTypeTable WHERE INT_PSEDO = 'SIMPLE')
-			BEGIN
-				SELECT
-				    RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER, DIS_NUM),
-					--INR_ID_INVOICE,	INS_ID,
-					IFD_ID_IFM,
-					CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
-					ELSE INR_GOOD END AS INR_GOOD,
-					--INR_GOOD,
-					INR_NAME, SO_INV_UNIT, SO_INV_OKEI, SUM(INR_SUM) AS INR_SUM,
-					SUM(INR_TNDS) AS INR_TNDS, SUM(INR_SNDS) AS INR_SNDS, SUM(INR_SALL) AS INR_SALL, SUM(INR_COUNT) AS INR_COUNT
-				FROM
-					#detail INNER JOIN
-					#master ON IFD_ID_IFM = IFM_ID INNER JOIN
-					#inv ON INV_ID = INS_ID
-				WHERE IFM_DATE = @date
-				GROUP BY IFD_ID_IFM, INR_GOOD, INR_NAME, SO_INV_UNIT, SO_INV_OKEI
-				ORDER BY SYS_ORDER, DIS_NUM
-			END
-			ELSE
-			BEGIN
-				SELECT
-				    RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER),
-					--INR_ID_INVOICE,	INS_ID,
-					IFD_ID_IFM,
-					--CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
-					--ELSE INR_GOOD END AS INR_GOOD,
-					INR_GOOD,
-					INR_NAME + ' (' + CONVERT(VARCHAR(20), COUNT(*)) + ' шт)' AS INR_NAME, SO_INV_UNIT, SO_INV_OKEI, SUM(INR_SUM) AS INR_SUM,
-					INR_TNDS, SUM(INR_SNDS) AS INR_SNDS, SUM(INR_SALL) AS INR_SALL,
-					SUM(INR_COUNT) AS INR_COUNT
-				FROM
-					#detail INNER JOIN
-					#master ON IFD_ID_IFM = IFM_ID INNER JOIN
-					#inv ON INV_ID = INS_ID
-				WHERE IFM_DATE = @date
-				GROUP BY
-					--INR_ID_INVOICE, INS_ID,
-					IFD_ID_IFM, INR_GOOD, INR_NAME, INR_TNDS,
-					SO_INV_UNIT, SO_INV_OKEI, SYS_ORDER--, CL_ID
-				ORDER BY SYS_ORDER
-			END
+			IF @Print_detail = 1
+			    IF (SELECT TOP 1 INS_ID_TYPE FROM #master) = (SELECT INT_ID FROM dbo.InvoiceTypeTable WHERE INT_PSEDO = 'SIMPLE')
+			    BEGIN
+				    SELECT
+				        RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER, DIS_NUM),
+				        MAX_RN= SUM(1) OVER (PARTITION BY IFD_ID_IFM),
+					    --INR_ID_INVOICE,	INS_ID,
+					    IFD_ID_IFM,
+					    CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
+					    ELSE INR_GOOD END AS INR_GOOD,
+					    --INR_GOOD,
+					    INR_NAME, SO_INV_UNIT, SO_INV_OKEI, SUM(INR_SUM) AS INR_SUM,
+					    SUM(INR_TNDS) AS INR_TNDS, SUM(INR_SNDS) AS INR_SNDS, SUM(INR_SALL) AS INR_SALL, SUM(INR_COUNT) AS INR_COUNT
+				    FROM
+					    #detail INNER JOIN
+					    #master ON IFD_ID_IFM = IFM_ID INNER JOIN
+					    #inv ON INV_ID = INS_ID
+				    WHERE IFM_DATE = @date
+				    GROUP BY IFD_ID_IFM, INR_GOOD, INR_NAME, SO_INV_UNIT, SO_INV_OKEI
+				    ORDER BY SYS_ORDER, DIS_NUM
+			    END
+			    ELSE
+			    BEGIN
+				    SELECT
+				        RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER),
+				        MAX_RN= SUM(1) OVER (PARTITION BY IFD_ID_IFM),
+					    --INR_ID_INVOICE,	INS_ID,
+					    IFD_ID_IFM,
+					    --CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
+					    --ELSE INR_GOOD END AS INR_GOOD,
+					    INR_GOOD,
+					    INR_NAME + ' (' + CONVERT(VARCHAR(20), COUNT(*)) + ' шт)' AS INR_NAME, SO_INV_UNIT, SO_INV_OKEI, SUM(INR_SUM) AS INR_SUM,
+					    INR_TNDS, SUM(INR_SNDS) AS INR_SNDS, SUM(INR_SALL) AS INR_SALL,
+					    SUM(INR_COUNT) AS INR_COUNT
+				    FROM
+					    #detail INNER JOIN
+					    #master ON IFD_ID_IFM = IFM_ID INNER JOIN
+					    #inv ON INV_ID = INS_ID
+				    WHERE IFM_DATE = @date
+				    GROUP BY
+					    --INR_ID_INVOICE, INS_ID,
+					    IFD_ID_IFM, INR_GOOD, INR_NAME, INR_TNDS,
+					    SO_INV_UNIT, SO_INV_OKEI, SYS_ORDER--, CL_ID
+				    ORDER BY SYS_ORDER
+			    END
 		END
 		ELSE
 		BEGIN
-			SELECT
-				IFM_ID,IFM_DATE,INS_ID,
-				--INS_ID_TYPE,
-				INT_PSEDO,
-				ORG_ID,ORG_PSEDO,ORG_FULL_NAME,ORG_SHORT_NAME,ORG_ADDRESS,ORG_INN,ORG_KPP,
-				INS_DATE,INS_NUM,INS_NUM_YEAR,
-				CL_ID,CL_PSEDO,CL_FULL_NAME,CL_SHORT_NAME,CL_INN,CL_KPP,
-				INS_CLIENT_ADDR,INS_CONSIG_NAME,INS_CONSIG_ADDR,INS_DOC_STRING,
-				ORG_DIR_SHORT,ORG_BUH_SHORT, INS_IDENT
-			FROM
-				#master AS IFM INNER JOIN
-				#inv ON INV_ID = INS_ID
-				LEFT JOIN dbo.InvoiceTypeTable AS ITP ON IFM.INS_ID_TYPE=ITP.INT_ID
-			WHERE IFM_DATE = @date
-			ORDER BY COUR_NAME, CL_PSEDO, CL_ID
+		    IF @Print_Master = 1
+			    SELECT
+				    IFM_ID,IFM_DATE,INS_ID,
+				    --INS_ID_TYPE,
+				    INT_PSEDO,
+				    ORG_ID,ORG_PSEDO,ORG_FULL_NAME,ORG_SHORT_NAME,ORG_ADDRESS,ORG_INN,ORG_KPP,
+				    INS_DATE,INS_NUM,INS_NUM_YEAR,
+				    CL_ID,CL_PSEDO,CL_FULL_NAME,CL_SHORT_NAME,CL_INN,CL_KPP,
+				    INS_CLIENT_ADDR,INS_CONSIG_NAME,INS_CONSIG_ADDR,INS_DOC_STRING,
+				    ORG_DIR_SHORT,ORG_BUH_SHORT, INS_IDENT, ACT_DATE, Convert(VarChar(20), ACT_DATE, 104) AS ACT_DATE_S
+			    FROM
+				    #master AS IFM INNER JOIN
+				    #inv ON INV_ID = INS_ID
+				    LEFT JOIN dbo.InvoiceTypeTable AS ITP ON IFM.INS_ID_TYPE=ITP.INT_ID
+			    WHERE IFM_DATE = @date
+			    ORDER BY COUR_NAME, CL_PSEDO, CL_ID
 
-			IF (SELECT TOP 1 INS_ID_TYPE FROM #master) = (SELECT INT_ID FROM dbo.InvoiceTypeTable WHERE INT_PSEDO = 'SIMPLE')
-			BEGIN
-				SELECT
-				    RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER, DIS_NUM),
-					--INR_ID_INVOICE,	INS_ID,
-					IFD_ID_IFM,
-					--CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
-					--ELSE INR_GOOD END AS INR_GOOD,
-					INR_GOOD,
-					INR_NAME, SO_INV_UNIT, SO_INV_OKEI, INR_SUM,
-					INR_TNDS, INR_SNDS, INR_SALL, INR_COUNT
-				FROM
-					#detail INNER JOIN
-					#master ON IFD_ID_IFM = IFM_ID INNER JOIN
-					#inv ON INV_ID = INS_ID
-				WHERE IFM_DATE = @date
-				ORDER BY SYS_ORDER, DIS_NUM
-			END
-			ELSE
-			BEGIN
-				SELECT
-				    RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER, DIS_NUM),
-					--INR_ID_INVOICE,	INS_ID,
-					IFD_ID_IFM,
-					--CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
-					--ELSE INR_GOOD END AS INR_GOOD,
-					INR_GOOD,
-					INR_NAME, SO_INV_UNIT, SO_INV_OKEI, SUM(INR_SUM) AS INR_SUM,
-					INR_TNDS, SUM(INR_SNDS) AS INR_SNDS, SUM(INR_SALL) AS INR_SALL,
-					INR_COUNT
-				FROM
-					#detail INNER JOIN
-					#master ON IFD_ID_IFM = IFM_ID INNER JOIN
-					#inv ON INV_ID = INS_ID
-				WHERE IFM_DATE = @date
-				GROUP BY
-					--INR_ID_INVOICE, INS_ID,
-					IFD_ID_IFM, INR_GOOD, INR_NAME, INR_ID_DISTR,
-					SO_INV_UNIT, SO_INV_OKEI, INR_TNDS, SYS_ORDER, DIS_NUM, INR_COUNT--, CL_ID
-				ORDER BY SYS_ORDER, DIS_NUM
-			END
+            IF @Print_Detail = 1
+			    IF (SELECT TOP 1 INS_ID_TYPE FROM #master) = (SELECT INT_ID FROM dbo.InvoiceTypeTable WHERE INT_PSEDO = 'SIMPLE')
+			    BEGIN
+				    SELECT
+				        RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER, DIS_NUM),
+				        MAX_RN= SUM(1) OVER (PARTITION BY IFD_ID_IFM),
+					    --INR_ID_INVOICE,	INS_ID,
+					    IFD_ID_IFM,
+					    --CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
+					    --ELSE INR_GOOD END AS INR_GOOD,
+					    INR_GOOD,
+					    INR_NAME, SO_INV_UNIT, SO_INV_OKEI, INR_SUM,
+					    INR_TNDS, INR_SNDS, INR_SALL, INR_COUNT
+				    FROM
+					    #detail INNER JOIN
+					    #master ON IFD_ID_IFM = IFM_ID INNER JOIN
+					    #inv ON INV_ID = INS_ID
+				    WHERE IFM_DATE = @date
+				    ORDER BY SYS_ORDER, DIS_NUM
+			    END
+			    ELSE
+			    BEGIN
+				    SELECT
+				        RN = Row_Number() Over(PARTITION BY IFD_ID_IFM ORDER BY SYS_ORDER, DIS_NUM),
+				        MAX_RN= SUM(1) OVER (PARTITION BY IFD_ID_IFM),
+					    --INR_ID_INVOICE,	INS_ID,
+					    IFD_ID_IFM,
+					    --CASE WHEN CL_ID = 5031 THEN 'Услуги пополнения экземпляров Систем КонсультантПлюс,установленных в подразделениях Заказчика объемом информации,который поступил Исполнителю от Разработчика Систем КонсультантПлюс с января по июль 2020г.включительно'
+					    --ELSE INR_GOOD END AS INR_GOOD,
+					    INR_GOOD,
+					    INR_NAME, SO_INV_UNIT, SO_INV_OKEI, SUM(INR_SUM) AS INR_SUM,
+					    INR_TNDS, SUM(INR_SNDS) AS INR_SNDS, SUM(INR_SALL) AS INR_SALL,
+					    INR_COUNT
+				    FROM
+					    #detail INNER JOIN
+					    #master ON IFD_ID_IFM = IFM_ID INNER JOIN
+					    #inv ON INV_ID = INS_ID
+				    WHERE IFM_DATE = @date
+				    GROUP BY
+					    --INR_ID_INVOICE, INS_ID,
+					    IFD_ID_IFM, INR_GOOD, INR_NAME, INR_ID_DISTR,
+					    SO_INV_UNIT, SO_INV_OKEI, INR_TNDS, SYS_ORDER, DIS_NUM, INR_COUNT--, CL_ID
+				    ORDER BY SYS_ORDER, DIS_NUM
+			    END
 		END
 
 
@@ -397,7 +416,7 @@ BEGIN
 						ORG_DIR_SHORT, ORG_BUH_SHORT,
 						CL_ID, CL_PSEDO, CL_FULL_NAME,
 						CL_SHORT_NAME, CL_KPP, CL_INN, INS_CLIENT_ADDR,
-						INS_CONSIG_ADDR, INS_CONSIG_NAME, INS_ID_TYPE, INS_IDENT
+						INS_CONSIG_ADDR, INS_CONSIG_NAME, INS_ID_TYPE, INS_IDENT, ACT_DATE
 					)
 			SELECT
 					IFM_DATE,
@@ -409,7 +428,7 @@ BEGIN
 						ORG_DIR_SHORT, ORG_BUH_SHORT,
 						CL_ID, CL_PSEDO, CL_FULL_NAME,
 						CL_SHORT_NAME, CL_KPP, CL_INN, INS_CLIENT_ADDR,
-						INS_CONSIG_ADDR, INS_CONSIG_NAME, INS_ID_TYPE, INS_IDENT
+						INS_CONSIG_ADDR, INS_CONSIG_NAME, INS_ID_TYPE, INS_IDENT, ACT_DATE
 				FROM	#master
 
 
@@ -452,7 +471,6 @@ BEGIN
 		EXEC [Maintenance].[ReRaise Error];
 	END CATCH
 END
-
 GO
 GRANT EXECUTE ON [dbo].[INVOICE_PRINT_BY_ID_LIST] TO rl_invoice_p;
 GO
