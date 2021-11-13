70b76283349ba31cb7ab721fc55fddb47cb18eb3
denisofff <hostsql@kprim.ru>
2021-11-13 22:28:09 +1000

    BuhDB. Определение автосгенерированной строки установки

--- BuhDB/StoredProcedures/dbo.ACT_DETAIL_PRINT.sql
+++ BuhDB/StoredProcedures/dbo.ACT_DETAIL_PRINT.sql
@@ -10,12 +10,21 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT
-	    SystemSet, SystemPrefix, SystemName, DistrTypeName, DistrNumber, NetVersion, DocCount,
-	    SystemPrice, TaxPrice, SystemPrice + TaxPrice AS TotalPrice, SystemNote,
-	    SystemExpire, IsGenerated
-	FROM ActSystemsTable WITH(NOLOCK)
-	WHERE ActID = @ACT AND NOT (SystemName LIKE '%Yubikey%' OR SystemName LIKE '%פכ‎ר%')
+    SELECT
+        SystemSet, SystemPrefix, SystemName, DistrTypeName, DistrNumber, NetVersion, DocCount,
+        SystemPrice, TaxPrice, TotalPrice, SystemNote,
+        SystemExpire, --IsGenerated
+        IsGenerated = Cast(CASE WHEN IsGenerated IS NOT NULL AND IsGenerated != 1 THEN 0  ELSE IsGenerated END AS Bit)
+    FROM
+    (
+	    SELECT
+	        SystemSet, SystemPrefix, SystemName, DistrTypeName, DistrNumber, NetVersion, DocCount,
+	        SystemPrice, TaxPrice, SystemPrice + TaxPrice AS TotalPrice, SystemNote, SystemOrder,
+	        SystemExpire, --IsGenerated
+	        IsGenerated = CASE WHEN SystemPrice = 60 THEN Row_Number() OVER(PARTITION BY SystemName, DistrNumber, SystemPrice ORDER BY SystemOrder) ELSE 0 END
+	    FROM ActSystemsTable WITH(NOLOCK)
+	    WHERE ActID = @ACT AND NOT (SystemName LIKE '%Yubikey%' OR SystemName LIKE '%פכ‎ר%')
+	) AS o_O
 	ORDER BY SystemOrder, DistrNumber, SystemPrice
 END
 
