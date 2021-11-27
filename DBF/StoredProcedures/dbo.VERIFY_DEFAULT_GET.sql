5258924afb98afd4c5943283aba4dcee0bc35c66
denisofff <hostsql@kprim.ru>
2021-11-27 22:12:49 +1000

    DBF. Акт сверки. Онлайн/оффлайн

--- DBF/StoredProcedures/dbo.VERIFY_DEFAULT_GET.sql
+++ DBF/StoredProcedures/dbo.VERIFY_DEFAULT_GET.sql
@@ -52,10 +52,45 @@ BEGIN
 								WHERE SL_ID_CLIENT = @clientid
 							) AS a
 					) AS O_O
-			) AS SL_REST
-		FROM
-			dbo.ClientTable LEFT OUTER JOIN
-			dbo.OrganizationTable ON CL_ID_ORG = ORG_ID
+			) AS SL_REST,
+			Cast (CASE
+			        WHEN EXISTS
+			            (
+			                SELECT TOP (1) *
+			                FROM dbo.DistrFinancingView AS D
+			                INNER JOIN dbo.SystemNetTable AS SN ON D.SN_ID = SN.SN_ID
+				            INNER JOIN dbo.SystemNetCountTable AS SNC ON SNC_ID_SN = SN.SN_ID
+				            WHERE SNC_TECH IN (3, 4, 6, 7, 9, 10, 11, 13)
+			                    AND D.CD_ID_CLIENT = @clientid
+			                    AND DSS_REPORT = 1
+			            )
+			            THEN 1
+			        ELSE 0
+			    END AS Bit)AS IsOnline,
+			CO_DATE, CO_NUM
+		FROM dbo.ClientTable
+		LEFT JOIN dbo.OrganizationTable ON CL_ID_ORG = ORG_ID
+		OUTER APPLY
+		(
+		    SELECT TOP (1) CO_DATE, CO_NUM
+		    FROM
+		    (
+		        SELECT TOP (1)
+		            1 AS TP, CO_NUM, CO_DATE
+		        FROM dbo.ContractTable
+		        WHERE CO_ID_CLIENT = CL_ID
+		            AND @date BETWEEN CO_BEG_DATE AND ISNULL(CO_END_DATE, '20500101')
+		        ORDER BY CO_DATE DESC, CO_ACTIVE DESC
+
+		        UNION ALL
+
+				SELECT TOP (1)
+				    2 AS TP, CO_NUM, CO_DATE
+				FROM dbo.ContractTable
+				WHERE CO_ID_CLIENT = CL_ID
+				    AND CO_ACTIVE = 1
+		    ) AS C
+		) AS C
 		WHERE CL_ID = @clientid
 
 		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = NULL;
