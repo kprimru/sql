77b940ea56d5c7fd69f2d73085f7ab7878b5dcb9
denisofff <hostsql@kprim.ru>
2021-12-21 04:22:44 +1000

    ClientDB. Права + мелкий рефакторинг

--- ClientDB/Tables/dbo.ActCalcDetail.sql
+++ ClientDB/Tables/dbo.ActCalcDetail.sql
@@ -6,7 +6,6 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[ActCalcDetail]
 (
-        [ID]          UniqueIdentifier      NOT NULL,
         [ID_MASTER]   UniqueIdentifier      NOT NULL,
         [SYS_REG]     NVarChar(128)         NOT NULL,
         [DISTR]       Int                   NOT NULL,
@@ -15,9 +14,7 @@ CREATE TABLE [dbo].[ActCalcDetail]
         [CONFRM]      Bit                       NULL,
         [CALC_NOTE]   NVarChar(512)             NULL,
         [CALC_DATE]   DateTime                  NULL,
-        CONSTRAINT [PK_dbo.ActCalcDetail] PRIMARY KEY CLUSTERED ([ID]),
+        CONSTRAINT [PK_ActCalcDetail] PRIMARY KEY CLUSTERED ([ID_MASTER],[SYS_REG],[DISTR],[COMP],[MON]),
         CONSTRAINT [FK_dbo.ActCalcDetail(ID_MASTER)_dbo.ActCalc(ID)] FOREIGN KEY  ([ID_MASTER]) REFERENCES [dbo].[ActCalc] ([ID])
 );
-GO
-CREATE NONCLUSTERED INDEX [IX_dbo.ActCalcDetail(ID_MASTER)+(SYS_REG,DISTR,COMP,MON)] ON [dbo].[ActCalcDetail] ([ID_MASTER] ASC) INCLUDE ([SYS_REG], [DISTR], [COMP], [MON]);
 GO
