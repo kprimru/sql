553551f28843338211022fec471b88d94d66d702
denisofff <hostsql@kprim.ru>
2022-05-01 01:30:19 +1000

    ClientDB. Карточка клиента. Информация техсектора

---
+++ ClientDB/StoredProcedures/dbo.Client=Tech@Select.sql
@@ -0,0 +1,44 @@
+﻿USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('[dbo].[Client=Tech@Select]', 'P ') IS NULL EXEC('CREATE PROCEDURE [dbo].[Client=Tech@Select]  AS SELECT 1')
+GO
+ALTER PROCEDURE [dbo].[Client=Tech@Select]
+	@Client_Id	Int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE
+		@DebugError		VarChar(512),
+		@DebugContext	Xml,
+		@Params			Xml;
+
+	DECLARE @AddressType_Id	UniqueIdentifier;
+
+	EXEC [Debug].[Execution@Start]
+		@Proc_Id		= @@ProcId,
+		@Params			= @Params,
+		@DebugContext	= @DebugContext OUT
+
+	BEGIN TRY
+		SELECT [Note]
+		FROM [dbo].[Client=Tech] AS T
+		WHERE T.[Client_Id] = @Client_Id;
+
+		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = NULL;
+	END TRY
+	BEGIN CATCH
+		SET @DebugError = Error_Message();
+
+		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = @DebugError;
+
+		EXEC [Maintenance].[ReRaise Error];
+	END CATCH
+END
+GO
+GRANT EXECUTE ON [dbo].[Client=Tech@Select] TO rl_client_tech;
+GO
---
+++ ClientDB/Tables/dbo.Client=Tech.sql
@@ -0,0 +1,14 @@
+﻿USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Client=Tech]
+(
+        [Client_Id]   Int               NOT NULL,
+        [Note]        VarChar(Max)          NULL,
+        CONSTRAINT [PK__Client=T__75A5D8F8CBA40FBF] PRIMARY KEY CLUSTERED ([Client_Id]),
+        CONSTRAINT [FK_Client=Tech_ClientTable] FOREIGN KEY  ([Client_Id]) REFERENCES [dbo].[ClientTable] ([ClientID])
+);
+GO
