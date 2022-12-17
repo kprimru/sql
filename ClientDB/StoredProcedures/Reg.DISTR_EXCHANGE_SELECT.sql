310ea99e162a7c3adc09d94544cf54afcc78be03
denisofff <hostsql@kprim.ru>
2022-12-17 23:28:12 +1000

    ClientDB. Отчет по заменам в РЦ

---
+++ ClientDB/StoredProcedures/Reg.DISTR_EXCHANGE_SELECT.sql
@@ -0,0 +1,70 @@
+﻿USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('[Reg].[DISTR_EXCHANGE_SELECT]', 'P ') IS NULL EXEC('CREATE PROCEDURE [Reg].[DISTR_EXCHANGE_SELECT]  AS SELECT 1')
+GO
+
+ALTER PROCEDURE [Reg].[DISTR_EXCHANGE_SELECT]
+    @DateFrom	SmallDateTime	= NULL,
+	@DateTo		SmallDateTime	= NULL,
+	@Distr		Int				= NULL,
+	@Client		VarChar(100)	= NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE
+        @DebugError     VarChar(512),
+        @DebugContext   Xml,
+        @Params         Xml;
+
+    EXEC [Debug].[Execution@Start]
+        @Proc_Id        = @@ProcId,
+        @Params         = @Params,
+        @DebugContext   = @DebugContext OUT
+
+    BEGIN TRY
+
+		IF Replace(@Client, '%', '') = ''
+			SET @Client = NULL;
+
+        IF @DateFrom IS NULL AND @DateTo IS NULL AND @Distr IS NULL AND @Client IS NULL
+			SET @DateFrom = DateAdd(Month, -2, GetDate());
+
+		SELECT
+			[DistrStr]		= R.[DistrStr],
+			[ClientID]		= C.[ClientID],
+			[ClientName]	= IsNull(C.[ClientFullName], R.[Comment]),
+			[ServiceName]	= C.[ServiceName],
+			[ManagerName]	= C.[ManagerName],
+			[OperDateTime]	= RH.[DATE],
+			[OperType]		= RH.[Type],
+			[OperValue]		= RH.[Value]
+		FROM [Reg].[RegHistoryOperationDetailView] AS RH
+		INNER JOIN [Reg].[RegDistr] AS RD ON RD.[ID] = RH.[ID_DISTR]
+		INNER JOIN [Reg].[RegNodeSearchView] AS R WITH(NOEXPAND) ON R.[HostID] = RD.[ID_HOST] AND R.[DistrNumber] = RD.[DISTR] AND R.[CompNumber] = RD.[COMP]
+		LEFT JOIN [dbo].[ClientDistrView] AS D ON D.[HostID] = RD.[ID_HOST] AND D.[DISTR] = RD.[DISTR] AND D.[COMP] = RD.[COMP]
+		LEFT JOIN [dbo].[ClientView] AS C WITH(NOEXPAND) ON C.[ClientID] = D.[ID_CLIENT]
+		WHERE (RH.[DATE] >= @DateFrom OR @DateFrom IS NULL)
+			AND (RH.[DATE] < @DateTo OR @DateTo IS NULL)
+			AND RH.Type IN ('Система', 'Сетевитость')
+			AND (R.[DistrNumber] = @Distr OR @Distr IS NULL)
+			AND (R.[Comment] LIKE @Client OR C.[ClientFullName] LIKE @Client OR @Client IS NULL)
+		ORDER BY RH.[DATE] DESC
+
+        EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = NULL;
+    END TRY
+    BEGIN CATCH
+        SET @DebugError = Error_Message();
+
+        EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = @DebugError;
+
+        EXEC [Maintenance].[ReRaise Error];
+    END CATCH
+END
+GO
+GRANT EXECUTE ON [Reg].[DISTR_EXCHANGE_SELECT] TO rl_reg_node_search;
+GO
---
+++ ClientDB/View/Reg.RegHistoryOperationDetailView.sql
@@ -0,0 +1,106 @@
+﻿USE [ClientDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('[Reg].[RegHistoryOperationDetailView]', 'V ') IS NULL EXEC('CREATE VIEW [Reg].[RegHistoryOperationDetailView]  AS SELECT 1')
+GO
+
+ALTER VIEW [Reg].[RegHistoryOperationDetailView]
+AS
+	SELECT
+		src.ID_DISTR, src.DATE, dst.ID,
+		C.Type, c.Value
+	FROM Reg.RegHistoryView AS SRC WITH(NOEXPAND)
+	CROSS APPLY
+    (
+		SELECT TOP (1)
+			ID,	ID_DISTR, DATE,
+			SystemShortName, NT_SHORT, SST_SHORT,
+			SUBHOST, TRAN_COUNT, TRAN_LEFT,
+			DS_NAME, DS_REG, REG_DATE, FIRST_REG, COMPLECT, COMMENT
+		FROM
+			Reg.RegHistoryView AS DST WITH(NOEXPAND)
+	    WHERE src.ID_DISTR = dst.ID_DISTR
+	        AND DST.DATE > SRC.DATE
+	    ORDER BY DST.DATE
+	) AS dst
+	CROSS APPLY
+	(
+		SELECT
+			[Type]		= 'Система',
+			[Value]		= 'с ' + src.SystemShortName + ' на ' + dst.SystemShortName
+		WHERE src.SystemShortName <> dst.SystemShortName
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Сетевитость',
+			[Value]		= 'с ' + src.NT_SHORT + ' на ' + dst.NT_SHORT
+		WHERE src.NT_SHORT <> dst.NT_SHORT
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Тип системы',
+			[Value]		= 'с ' + src.SST_SHORT + ' на ' + dst.SST_SHORT
+		WHERE src.SST_SHORT <> dst.SST_SHORT
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Признак подхоста',
+			[Value]		= 'с ' + CASE WHEN src.SUBHOST = 1 THEN 'Да' ELSE 'Нет' END + ' на ' + CASE WHEN dst.SUBHOST = 1 THEN 'Да' ELSE 'Нет' END
+		WHERE src.SUBHOST <> dst.SUBHOST
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Кол-во переносов',
+			[Value]		= 'с ' + Cast(src.TRAN_COUNT AS VarChar(10)) + ' на ' + Cast(dst.TRAN_COUNT AS VarChar(10))
+		WHERE src.TRAN_COUNT <> dst.TRAN_COUNT
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Осталось счетчиков',
+			[Value]		= 'с ' + Cast(src.TRAN_LEFT AS VarChar(10)) + ' на ' + Cast(dst.TRAN_LEFT AS VarChar(10))
+		WHERE src.TRAN_LEFT <> dst.TRAN_LEFT
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Статус',
+			[Value]		= 'с ' + src.DS_NAME + ' на ' + dst.DS_NAME
+		WHERE src.DS_NAME <> dst.DS_NAME
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Дата регистрации',
+			[Value]		= 'с ' + Convert(VarChar(20), src.REG_DATE, 104) + ' на ' + Convert(VarChar(20), dst.REG_DATE, 104)
+		WHERE IsNull(src.REG_DATE, '1900-01-01') <> IsNull(dst.REG_DATE, '1900-01-01')
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Дата первой регистрации',
+			[Value]		= 'с ' + IsNull(Convert(VarChar(20), src.FIRST_REG, 104), '-') + ' на ' + IsNull(Convert(VarChar(20), dst.FIRST_REG, 104), '-')
+		WHERE IsNull(src.FIRST_REG, '1900-01-01') <> IsNull(dst.FIRST_REG, '1900-01-01')
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Комплект',
+			[Value]		= 'с ' + ISNULL(src.COMPLECT, '') + ' на ' + ISNULL(dst.COMPLECT, '')
+		WHERE ISNULL(src.COMPLECT, '') <> ISNULL(dst.COMPLECT, '')
+		---
+		UNION ALL
+		---
+		SELECT
+			[Type]		= 'Примечание',
+			[Value]		= 'с ' + ISNULL(src.COMMENT, '') + ' на ' + ISNULL(dst.COMMENT, '')
+		WHERE ISNULL(src.COMMENT, '') <> ISNULL(dst.COMMENT, '')
+	) AS C
+GO
