041d1190fdbe2fb50d3447c413860ec530cd6b3b
denisofff <hostsql@kprim.ru>
2023-02-21 15:51:19 +1000

    ClientDB. Отчет по неподписанным актам. Выводить руководителя

--- ClientDB/StoredProcedures/Report.DBF_ACT_UNSIGN.sql
+++ ClientDB/StoredProcedures/Report.DBF_ACT_UNSIGN.sql
@@ -41,12 +41,29 @@ BEGIN
         EXEC [DBF].[dbo].[ACT_UNSIGN_REPORT] @Date;
 
         SELECT
-            [Клиент]    = [Psedo],
-            [СИ]        = [ServiceName],
-            [Дата акта] = [ActDate],
-            [Месяц]     = [PeriodDate]
-        FROM @Result
-        ORDER BY 2, 1, 4, 3
+            [Клиент]		= [Psedo],
+            [СИ]			= [ServiceName],
+			[Руководитель]	= C.[ManagerName],
+            [Дата акта]		= [ActDate],
+            [Месяц]			= [PeriodDate]
+        FROM @Result AS R
+		OUTER APPLY
+		(
+			SELECT TOP (1) D.[DIS_NUM], D.[DIS_COMP_NUM], D.[SYS_REG_NAME]
+			FROM [DBF].[dbo.ClientDistrView] AS D
+			WHERE D.[CD_ID_CLIENT] = R.[Id]
+			ORDER BY D.[DSS_REPORT] DESC, D.[SYS_ORDER]
+		) AS CD
+		OUTER APPLY
+		(
+			SELECT TOP (1) C.[ManagerName]
+			FROM [dbo].[ClientDistrView] AS D WITH(NOEXPAND)
+			INNER JOIN [dbo].[ClientView] AS C WITH(NOEXPAND) ON C.[ClientID] = D.[ID_CLIENT]
+			WHERE D.[SystemBaseName] = CD.[SYS_REG_NAME]
+				AND D.[DISTR] = CD.[DIS_NUM]
+				AND D.[COMP] = CD.[DIS_COMP_NUM]
+		) AS C
+        ORDER BY 2, 3, 1, 5, 4;
 
 		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = NULL;
 	END TRY
