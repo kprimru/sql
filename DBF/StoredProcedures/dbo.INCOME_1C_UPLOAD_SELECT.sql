1d045baa9e711f4dcf23ff655d6b7061494502a4
workprots <workprots@gmail.com>
2023-05-29 17:37:04 +1000

    Доработал выгрузку 1С

--- DBF/StoredProcedures/dbo.INCOME_1C_UPLOAD_SELECT.sql
+++ DBF/StoredProcedures/dbo.INCOME_1C_UPLOAD_SELECT.sql
@@ -25,47 +25,47 @@ BEGIN
 		@DebugContext	= @DebugContext OUT
 
 	BEGIN TRY
-
 		SELECT
-		   [Номер] = I.[IN_PAY_NUM],
-		   [Дата] = Convert(VarChar(20), I.[IN_PAY_DATE], 104),
-		   [Сумма] = I.[IN_SUM],
-		   [ПлательщикСчет] = C.[CL_ACCOUNT],
-		   [ДатаСписано] = NULL,
-		   [Плательщик] = C.[CL_FULL_NAME],
-		   [ПлательщикИНН] = C.[CL_INN],
-		   [ПлательщикРасчСчет] = C.[CL_ACCOUNT],
-		   [ПлательщикБанк1] = B.BA_NAME,
-		   B.[BA_LORO],
-		   B.[BA_BIK],
-		   O.ORG_ACCOUNT,
-		   I.IN_DATE,
-		   O.ORG_SHORT_NAME,
-		   O.ORG_INN,
-		   OB.BA_NAME,
-		   [ВидПлатежа] = 'Электронно',
-		   [НазначениеПлатежа] = 'Оплата за ' + T.PR_NAME + ' года. В том числе НДС ' + Cast(Floor(T.TX_PERCENT) AS VarChar(20)) + ' % - ' +   dbo.MoneyFormat(IN_SUM / (100 + TX_PERCENT) / 100) + ' рублей.'
-		FROM dbo.IncomeTable                                        AS I
-		INNER JOIN dbo.ClientTable                                AS C ON CL_ID = IN_ID_CLIENT
-		OUTER APPLY
+			O.[ORG_FULL_NAME],
+			[Номер] = I.[IN_PAY_NUM],
+			[Дата] = Convert(VarChar(20), I.[IN_PAY_DATE], 104),
+			[Сумма] = I.[IN_SUM],
+			[ПлательщикСчет] = C.[CL_ACCOUNT],
+			[ДатаСписано] = NULL,
+			[Плательщик] = C.[CL_FULL_NAME],
+			[ПлательщикИНН] = C.[CL_INN],
+			[ПлательщикРасчСчет] = C.[CL_ACCOUNT],
+			[ПлательщикБанк1] = B.BA_NAME,
+			B.[BA_LORO], --[ПлательщикКорсчет] =
+			B.[BA_BIK], --[ПлательщикБИК] =
+			O.ORG_ACCOUNT, --[ПолучательСчет] =
+			I.IN_DATE, --[ДатаПоступило] =
+			O.ORG_SHORT_NAME, --[Получатель] =
+			O.ORG_INN, --[ПолучательИНН] =
+			OB.BA_NAME, --[ПолучательБанк1] =
+			[ВидПлатежа] = 'Электронно',
+			[НазначениеПлатежа] = 'Оплата за ' + T.PR_NAME + ' года. В том числе НДС ' + Cast(Floor(T.TX_PERCENT) AS VarChar(20)) + ' % - ' +   dbo.MoneyFormat(IN_SUM - (IN_SUM / ((100 + TX_PERCENT) / 100))) + ' рублей.'
+		FROM dbo.IncomeTable				AS I
+			INNER JOIN dbo.ClientTable			AS C ON CL_ID = IN_ID_CLIENT
+			OUTER APPLY
 		(
-			   SELECT TOP (1)
-					   TX_PERCENT,
-					   PR_NAME
-			   FROM dbo.IncomeDistrTable                        AS ID
-			   INNER JOIN dbo.DistrView                        AS D WITH(NOEXPAND) ON DIS_ID = ID_ID_DISTR
-			   INNER JOIN dbo.SaleObjectTable                        AS SO ON SYS_ID_SO = SO_ID
-			   INNER JOIN dbo.PeriodTable                                AS P ON P.PR_ID = ID.ID_ID_PERIOD
-			   INNER JOIN dbo.TaxTable                                        AS T ON SO_ID_TAX = TX_ID
-			   WHERE IN_ID = ID_ID_INCOME
+			SELECT TOP (1)
+				TX_PERCENT,
+				PR_NAME
+			FROM dbo.IncomeDistrTable					AS ID
+				INNER JOIN dbo.DistrView				AS D WITH(NOEXPAND) ON DIS_ID = ID_ID_DISTR
+				INNER JOIN dbo.SaleObjectTable			AS SO ON SYS_ID_SO = SO_ID
+				INNER JOIN dbo.PeriodTable				AS P ON P.PR_ID = ID.ID_ID_PERIOD
+				INNER JOIN dbo.TaxTable					AS T ON SO_ID_TAX = TX_ID
+			WHERE IN_ID = ID_ID_INCOME
 		) AS T
-		INNER JOIN dbo.OrganizationTable                AS O ON O.ORG_ID = I.IN_ID_ORG
-		LEFT JOIN dbo.BankTable                                        AS B ON B.BA_ID = C.[CL_ID_BANK]
-		LEFT JOIN dbo.BankTable                                        AS OB ON OB.BA_ID = O.[ORG_ID_BANK]
+			INNER JOIN dbo.OrganizationTable		AS O ON O.ORG_ID = I.IN_ID_ORG
+			LEFT JOIN dbo.BankTable					AS B ON B.BA_ID = C.[CL_ID_BANK]
+			LEFT JOIN dbo.BankTable					AS OB ON OB.BA_ID = O.[ORG_ID_BANK]
 		WHERE
-		   IN_DATE = @BEGIN --'20230518'
-		   AND
-		   IN_ID_ORG = @ORG--1
+			IN_DATE = @BEGIN --'20230518'
+			AND
+			IN_ID_ORG = @ORG--1
 
 		EXEC [Debug].[Execution@Finish] @DebugContext = @DebugContext, @Error = NULL;
 	END TRY
